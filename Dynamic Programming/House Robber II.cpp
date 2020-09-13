a1431fd2146c11360aa1a699215de99d6c3f5980
Arjun Grover <45965687+iamarjun45@users.noreply.github.com>
2020-09-13 21:43:04 +0530

    Add files via upload

---
+++ Dynamic Programming/House Robber II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        if(nums.size()==1)
+            return nums[0];
+        vector<int> dp1(nums.size()),dp2(nums.size());
+        int m1=max(nums[0],nums[1]);
+        if(nums.size()==2)
+            return m1;
+        dp1[0]=nums[0];
+        dp1[1]=max(nums[0],nums[1]);
+        
+        dp2[1]=nums[1];
+        dp2[2]=max(nums[1],nums[2]);
+        
+        
+        for(int i=2;i<nums.size()-1;i++){
+            dp1[i] = max(dp1[i-2]+nums[i],dp1[i-1]);
+            m1=max(m1,dp1[i]);
+        }
+        for(int i=3;i<nums.size();i++){
+            dp2[i] = max(dp2[i-2]+nums[i],dp2[i-1]);
+        }
+        
+        return max(dp1[nums.size()-2],dp2[nums.size()-1]);
+    }
+};
