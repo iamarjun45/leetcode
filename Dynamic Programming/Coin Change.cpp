365bab464a79f02a81dcf11e416955247cb82802
Arjun Grover <45965687+iamarjun45@users.noreply.github.com>
2020-09-14 23:29:42 +0530

    Add files via upload

---
+++ Dynamic Programming/Coin Change.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,INT_MAX-1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(auto coin:coins){
+                if(coin<=i)
+                    dp[i]=min(dp[i],dp[i-coin]+1);
+            }
+        }
+        return (dp[amount]==INT_MAX-1? -1: dp[amount]);
+    }
+};
