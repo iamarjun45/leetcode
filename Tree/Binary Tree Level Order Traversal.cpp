3051c6c50308f8f6909f36160183c36dce042d7b
Arjun Grover <45965687+iamarjun45@users.noreply.github.com>
2020-09-16 22:08:46 +0530

    Add files via upload

---
+++ Tree/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==NULL)
+            return ans;
+        queue<TreeNode*> pendingNodes;
+        pendingNodes.push(root);
+        pendingNodes.push(NULL);
+        vector<int> small;
+        while(pendingNodes.size()!=0){
+            TreeNode* front=pendingNodes.front();
+            pendingNodes.pop();
+            if(front==NULL){
+                ans.push_back(small);
+                small=vector<int>();
+                if(pendingNodes.size()==0)
+                    break;
+                pendingNodes.push(NULL);
+            }else{
+                small.push_back(front->val);
+                if(front->left)
+                    pendingNodes.push(front->left);
+                if(front->right)
+                    pendingNodes.push(front->right);
+            }
+        }
+        return ans;
+    }
+};
